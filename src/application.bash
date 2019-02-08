8c490c958b4b8ff482b9670142bf8bbcb757c9fb
GUINAIS Romain <rguinais@iia-laval.fr>
2019-02-08 10:59:21 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,6 @@
-###Application.bash - 2019 - GUINAIS###
-echo "Copyright Company - 2019"
-echo "Build : 10:00"
-echo "version 1"
+### Application.bash - 2019 - GUINAIS ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
